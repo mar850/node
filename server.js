48078ae50e38c3c682329edef1f2ac4afe64f545
marcinsz <mar850@wp.pl>
2015-02-21 11:57:06 +0100

    Ustawieni prodprotololu na null w websockecie

--- server.js
+++ server.js
@@ -125,21 +125,23 @@ function originIsAllowed(origin) {
 }
 
 wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin 
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+    // if (!originIsAllowed(request.origin)) {
+    //   // Make sure we only accept requests from an allowed origin 
+    //   request.reject();
+    //   console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    //   return;
+    // }
     
-    var connection = request.accept('echo-protocol', request.origin);
+    var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
+            console.log('przygotowanie do wywylania wiadomosci')
             console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
         }
         else if (message.type === 'binary') {
+
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
             connection.sendBytes(message.binaryData);
         }
